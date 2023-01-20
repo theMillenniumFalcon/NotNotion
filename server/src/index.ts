8f4dea221fe55aa85c63c96ec3c7764b8d28e89d
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-20 23:34:15 +0530

    added cookie ppareser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv-safe/config'
 import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+
 import { connectDB } from './database/database-connection'
 
 const PORT = process.env.PORT
@@ -9,6 +12,8 @@ const main = async () => {
     await connectDB()
 
     const app = express()
+
+    app.set("trust proxy", 1)
 
     app.use((_req, res, next) => {
         res.setHeader("Access-Control-Allow-Headers", "Content-Type")
@@ -21,10 +26,19 @@ const main = async () => {
         next()
     })
 
-    app.set("trust proxy", 1)
+    app.use(bodyParser.json())
+    app.use(cookieParser())
 
     app.get('/', (_, res) => {
         res.send("Server is working fine!")
+    })
+
+    app.use(({ err, res }: any) => {
+        console.log(err)
+        const status = err.statusCode || 500
+        const message = err.message
+        const data = err.data
+        res.status(status).json({ message: message, errCode: status, data: data })
     })
 
     const server = app.listen(PORT, () => {

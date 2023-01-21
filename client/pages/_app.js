ab9e92516e250ea94667bb9dfe1ab33973de0bf2
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-21 15:44:09 +0530

    layout, context, button added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,33 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import App from "next/app"
+import cookies from "next-cookies"
+
+import UserProvider from "../context/UserContext"
+import Layout from "../components/layout"
+
+import "typeface-nunito-sans"
+import "typeface-roboto"
+import "../shared/global.scss"
+
+const MyApp = ({ Component, pageProps, isAuthenticated }) => {
+  return (
+    <UserProvider isAuthenticated={isAuthenticated}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  )
 }
+
+MyApp.getInitialProps = async (context) => {
+  let isAuthenticated = false
+
+  const { token } = cookies(context.ctx)
+  if (token) {
+    isAuthenticated = true
+  }
+
+  const appProps = await App.getInitialProps(context)
+  return { ...appProps, isAuthenticated }
+}
+
+export default MyApp

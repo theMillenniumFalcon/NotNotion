23a24e6190f11ff11609fc14b38b53d3046a53fe
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-24 15:41:24 +0530

    added utils

---
+++ client/utils/objectId.js
@@ -0,0 +1,14 @@
+// Helper function to generate MongoDB-like ObjectIds
+const objectId = () => {
+    const timestamp = ((new Date().getTime() / 1000) | 0).toString(16)
+    return (
+        timestamp +
+        "xxxxxxxxxxxxxxxx"
+            .replace(/[x]/g, function () {
+                return ((Math.random() * 16) | 0).toString(16)
+            })
+            .toLowerCase()
+    )
+}
+
+export default objectId
